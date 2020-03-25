1f624b1d1cc4269367c0633663cc57bc93b0df2e
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-24 22:43:45 -0400

    added images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         name: "Julie",
-        photo: " ",
+        photo: "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=339&q=80",
         scores: [
             "1",
             "3",
@@ -17,7 +17,7 @@ var friends = [
     },
     {
         name: "Adam",
-        photo: " ",
+        photo: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=360&q=80",
         scores: [
             "3",
             "2",
@@ -33,7 +33,7 @@ var friends = [
     },
     {
         name: "Mary",
-        photo: " ",
+        photo: "https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         scores: [
             "5",
             "2",
@@ -49,7 +49,7 @@ var friends = [
     },
     {
         name: "John",
-        photo: " ",
+        photo: "https://images.unsplash.com/photo-1544568100-847a948585b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
         scores: [
             "3",
             "5",
@@ -65,7 +65,7 @@ var friends = [
     },
     {
         name: "Danielle",
-        photo: " ",
+        photo: "https://images.unsplash.com/photo-1530126483408-aa533e55bdb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=401&q=80",
         scores: [
             "5",
             "2",
@@ -81,7 +81,7 @@ var friends = [
     },
     {
         name: "Andrew",
-        photo: " ",
+        photo: "https://images.unsplash.com/photo-1505628346881-b72b27e84530?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
         scores: [
             "4",
             "2",
